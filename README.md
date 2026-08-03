d137fde0a56ff3e129e6b634037a5c3224ef58f7
motionlab <tatianageorgievna26@gmail.com>
2026-08-03 12:25:26 +0300

    Add files via upload

--- README.md
+++ README.md
@@ -1,11 +1,3 @@
-# English with Tanya
-
-Загрузите все файлы из этой папки в корень репозитория GitHub.
-
-Для GitHub Pages:
-1. Settings
-2. Pages
-3. Deploy from a branch
-4. Branch: main
-5. Folder: / (root)
-6. Save
+# English with Tanya v3
+Загрузите все файлы в корень того же репозитория с заменой старых файлов.
+После обновления закройте приложение на телефоне полностью и откройте снова.
--- service-worker.js
+++ service-worker.js
@@ -1,5 +1,1 @@
-const CACHE='english-tanya-v1';
-const ASSETS=['./','./index.html','./manifest.webmanifest','./icon-192.png','./icon-512.png','./apple-touch-icon.png','./favicon.png'];
-self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS))));
-self.addEventListener('activate',e=>e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k))))));
-self.addEventListener('fetch',e=>e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request))));
+const CACHE='english-tanya-v3';const A=['./','./index.html','./manifest.webmanifest','./icon-192.png','./icon-512.png','./apple-touch-icon.png','./favicon.png'];self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(A))));self.addEventListener('activate',e=>e.waitUntil(caches.keys().then(k=>Promise.all(k.filter(x=>x!==CACHE).map(x=>caches.delete(x))))));self.addEventListener('fetch',e=>e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request))));
